575599967959112c6a224583ce29a2875fb1d495
Hemanth T C <hemanthtc8296@gmail.com>
2026-08-21 01:11:44 +0530

    chore: remove docker, railway, and cdk files for native aws deployment

--- amplify/custom/fargate-resource.d.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-/**
- * Custom CDK Stack to deploy the AnyControl Remote backend to AWS ECS/Fargate.
- */
-export declare class FargateStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps);
-}
